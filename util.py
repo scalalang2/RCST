1358d79d186eba133b5d3b6b1e0c53f1c3587340
scalalang2 <cailhuiris@gmail.com>
2021-01-07 14:29:10 +0900

    fix: function name

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
-def account_to_shard(address: str, number_of_shard):
-    return int(address, 16) % number_of_shard
+def account_to_group(address: str, number_of_group):
+    return int(address, 16) % number_of_group
 
 
 if __name__ == "__main__":
-    print(account_to_shard("0x043f3579201977dfed8ae46bc9f1561c92b6cfac",20))
+    print(account_to_group("0x043f3579201977dfed8ae46bc9f1561c92b6cfac",100))
