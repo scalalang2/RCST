47ac88f3673f64d3d0616323ccc205aefcb420a9
DoHyeon Lee <cailhuiris@gmail.com>
2021-01-09 20:57:28 +0900

    updated

--- main.py
+++ main.py
@@ -6,21 +6,45 @@ simulator = Simulator({
     "block_to_read": 50,
     "collation_cycle": 100,
     "account_group": 100,
-    "number_of_shard": 12,
+    "number_of_shard": 20,
     "gas_limit": 12000000,
     "gas_cross_shard_tx": 21000,
 })
 
-# sacc = SACC()
+sacc = SACC()
 # simulator.simulate(balancer=sacc)
-# simulator.simulate(balancer=sacc, with_cstx=True)
+simulator.simulate(balancer=sacc, with_cstx=True)
 
 garet = GARET(relocation_cycle=5)
 # simulator.simulate(balancer=garet)
 simulator.simulate(balancer=garet, with_cstx=True)
 
+garet = GARET(relocation_cycle=10)
+# simulator.simulate(balancer=garet)
+simulator.simulate(balancer=garet, with_cstx=True)
+
+garet = GARET(relocation_cycle=15)
+# simulator.simulate(balancer=garet)
+simulator.simulate(balancer=garet, with_cstx=True)
+
 balance_meter = BalanceMeter(
     relocation_cycle=5,
+    w_tx=0.0,
+    w_gas=0.7,
+    w_cross_tx=0.3
+)
+simulator.simulate(balancer=balance_meter, with_cstx=True)
+
+balance_meter = BalanceMeter(
+    relocation_cycle=10,
+    w_tx=0.0,
+    w_gas=0.7,
+    w_cross_tx=0.3
+)
+simulator.simulate(balancer=balance_meter, with_cstx=True)
+
+balance_meter = BalanceMeter(
+    relocation_cycle=15,
     w_tx=0.0,
     w_gas=0.7,
     w_cross_tx=0.3
